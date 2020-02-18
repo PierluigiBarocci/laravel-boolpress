4e8b78abccaa7f436abe8d57db39aea37230b4e4
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-02-18 16:01:21 +0100

    View Home Pubblica e View Admin

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -21,7 +21,7 @@
 </head>
 <body>
     <div id="app">
-        @include('layouts.partials.navbar')
+        @include('layouts.partials.public-navbar')
 
         <main class="py-4">
             @yield('content')
